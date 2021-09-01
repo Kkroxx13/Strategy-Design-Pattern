6475f6c6483e04d44992ed66e0a8d2778974ce7b
Karan Kanchan <karan.kanchan@morningstar.com>
2021-09-01 22:23:49 +0530

    Implemented Strategy Design Pattern

---
+++ StrategyDesignPattern/IStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrategyDesignPattern
+{
+    public interface IStrategy
+    {
+        public int DoOperation(int num1, int num2);
+        
+        
+    }
+}
---
+++ StrategyDesignPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StrategyDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Context context = new Context(new AddOperation());
+            Console.WriteLine("10 + 5 ="+context.executeStrategy(10,5));
+            Console.WriteLine();
+
+            context = new Context(new SubtractOperation());
+            Console.WriteLine("10 - 5 =" + context.executeStrategy(10, 5));
+            Console.WriteLine();
+
+            context = new Context(new MultiplyOperation());
+            Console.WriteLine("10 * 5 =" + context.executeStrategy(10, 5));
+            Console.WriteLine();
+        }
+    }
+}
